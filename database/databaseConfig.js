d369475773bc22c34c86155eb579b0068273ef87
precious <arierhiprecious@gmail.com>
2023-07-09 16:08:39 -0700

    'first-version'

--- database/databaseConfig.js
+++ database/databaseConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.DB_LOCAL).then(() => {
+mongoose.connect(process.env.DB_STRING).then(() => {
     console.log("connected to database")
 })
 
